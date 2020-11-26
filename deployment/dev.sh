b10ec5efa8b9ddeeb0e58d5a284f7620925be076
Tom Reed <t.reed1104@gmail.com>
2020-11-26 08:59:30 +0000

    Configured deployment scripts

--- deployment/dev.sh
+++ deployment/dev.sh
@@ -2,11 +2,11 @@
 echo ---------------------------------------
 echo Deployment Start - Development
 echo ---------------------------------------
-echo Building and Deploying Vue App
+echo Building and Deploying Heimdall Frontend
 echo ---------------------------------------
-docker-compose -p vue-app-dev -f docker-compose.dev.yml up -d --build --remove-orphans
+docker-compose -p heimdall-frontend-dev -f docker-compose.dev.yml up -d --build --remove-orphans
 echo
 echo ---------------------------------------
 echo Conntainer Status:
 echo ---------------------------------------
-docker ps | grep 'vue-app'
+docker ps | grep 'heimdall-frontend'
