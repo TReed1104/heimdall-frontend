6449bac8b64b53a0a0e953909162b11d657d0fd3
Tom Reed <t.reed1104@gmail.com>
2020-11-26 09:09:30 +0000

    Migrated the vuex store

--- app/src/store/store.js
+++ app/src/store/store.js
@@ -5,8 +5,15 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
+        // The address of the server hosting the Heimdall API
+        serverAddress: "http://127.0.0.1/heimdall-api",
     },
     getters: {
+        // Endpoints
+        heimdallServerAddress: state => state.serverAddress,
+        heimdallEndpointRooms: state => { return state.serverAddress + "/rooms" },
+        heimdallEndpointComputers: state => { return state.serverAddress + "/computers" },
+        heimdallEndpointAvailability: state => { return state.serverAddress + "/availability?room=" },
     },
     mutations: {
     },
