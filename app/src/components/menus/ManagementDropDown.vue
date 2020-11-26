2975b1ecf347e692d4d02e387bbf0e7abd664b08
Tom Reed <t.reed1104@gmail.com>
2020-11-26 09:10:05 +0000

    Migrated menu components

---
+++ app/src/components/menus/ManagementDropDown.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="dropdown">
+        <a class="nav-item nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Management
+        </a>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <router-link tag="a" class="dropdown-item" :to="{name: 'Rooms List'}">Rooms</router-link>
+            <router-link tag="a" class="dropdown-item" :to="{name: 'Desks List'}">Desks</router-link>
+            <router-link tag="a" class="dropdown-item" :to="{name: 'Monitoring'}">Computers</router-link>
+            <router-link tag="a" class="dropdown-item" :to="{name: 'Operating System List'}">OS</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
