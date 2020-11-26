7f55e6a267c828cb61f2b15b91ea65a01414cf23
Tom Reed <t.reed1104@gmail.com>
2020-11-26 09:04:09 +0000

    Converted Home and App core vue components

--- app/src/components/Home.vue
+++ app/src/components/Home.vue
@@ -1,17 +1,14 @@
 <template>
     <div class="container">
-        <h1 class="page-title">Webpack Template</h1>
-        <p>A project template for web projects using Webpack, Babel, Vue, Sass and Bootstrap. The template supports both Javascript and Typescript as usable languages.</p>
+        <h1 class="page-title">Heimdall, The Watchman</h1>
+        <p>Welcome to Heimdall, the lab monitoring service. Heimdall is an open-source project designed and developed by the School of Computer Science Technical team at the University of Lincoln.<br/><br/>
+        The purpose of the project was to provide a cloud based REST API to provide remote monitoring to the team, allowing them to ensure their facilities were in a fully working order.
+        This is achieved by recording a collection of system data about each machine registered with the system, which is then interpretted and fed to the team to notify them remotely of any changes or issues.<br/><br/>
+        During the intial iteration of the system, it was identified that the recorded data could also be used to provide a "Find-a-PC" application to our students, allowing them to always be able to see where PCs are available for use in our computing labs.</p>
     </div>
 </template>
 
 <script>
-    //import JavascriptTemplate from './TemplateJavascript.vue';
-    //import TypescriptTemplate from "./TemplateTypescript.vue";
     export default {
-        components: {
-            //JavascriptTemplate,
-            //TypescriptTemplate
-        }
     }
 </script>
